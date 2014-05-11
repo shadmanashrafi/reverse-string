877f40d4d4a64899ba57240dcb39c127bc919f0b
shadman <shadman@ubuntu.(none)>
2014-05-11 18:02:32 +0530

    Initial commit

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+class ReverseString{
+public static void main(String args[]){
+
+String rev="shadman";
+
+for(int i=rev.length()-1;i>=0;i--)
+{
+System.out.print(rev.charAt(i));
+}
+System.out.println();
+}
+}
